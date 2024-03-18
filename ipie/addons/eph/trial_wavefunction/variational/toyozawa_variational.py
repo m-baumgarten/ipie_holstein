d1b1e888c5dd731017a58bb397f7426a1c5dcc0d
Moritz Baumgarten <mbaumgarten@fas.harvard.edu>
2024-03-18 10:58:34 -0400

    k-point toyozawa

--- ipie/addons/eph/trial_wavefunction/variational/toyozawa_variational.py
+++ ipie/addons/eph/trial_wavefunction/variational/toyozawa_variational.py
@@ -13,24 +13,23 @@
 # limitations under the License.
 
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize, basinhopping
 from ipie.addons.eph.trial_wavefunction.variational.estimators import gab
 
 import jax
 import jax.numpy as npj
 
 
-def gradient_toyozawa_mo(x, nbasis, nup, ndown, T, U, g, m, w0, perms, restricted):
+def gradient_toyozawa_mo(x, nbasis, nup, ndown, T, U, g, m, w0, perms, coeffs, restricted):
     grad = np.array(
         jax.grad(objective_function_toyozawa_mo)(
-            x, nbasis, nup, ndown, T, U, g, m, w0, perms, restricted
+            x, nbasis, nup, ndown, T, U, g, m, w0, perms, coeffs, restricted
         )
     )
     return grad
 
-
 def objective_function_toyozawa_mo(
-    x, nbasis, nup, ndown, T, U, g, m, w0, perms, restricted, zero_th=1e-12
+    x, nbasis, nup, ndown, T, U, g, m, w0, perms, coeffs, restricted, zero_th=1e-12
 ):
     nbasis = int(round(nbasis))
     nup = int(round(nup))
@@ -55,9 +54,105 @@ def objective_function_toyozawa_mo(
     #beta0 = shift0 * npj.sqrt(m * w0 / 2.0)
     beta0 = shift0
 
-    for perm in perms:
-        psia_j = psi0a[perm, :]
+    for ip, (permi, coeffi) in enumerate(zip(perms, coeffs)):
+        beta_i = beta0[npj.array(permi)]
+        psia_i = psi0a[permi, :]
+
+        if noccb > 0:
+            psib_i = psi0b[permi, :]
+
+        for jp, (permj, coeffj) in enumerate(zip(perms, coeffs)):
+            beta_j = beta0[npj.array(permj)]
+            psia_j = psi0a[permj, :]
+    #        psia_j *= coeff
+
+            if noccb > 0:
+                psib_j = psi0b[permj, :]
+                overlap = (
+                    npj.linalg.det(psia_i.T.dot(psia_j))
+                    * npj.linalg.det(psib_i.T.dot(psib_j))
+                    * npj.prod(npj.exp(-0.5 * (beta_i**2 + beta_j**2) + beta_i * beta_j))
+                )
+            else:
+                overlap = npj.linalg.det(psia_i.T.dot(psia_j)) * npj.prod(
+                    npj.exp(-0.5 * (beta_i**2 + beta_j**2) + beta_i * beta_j)
+                )
+            overlap *= coeffi.conj() * coeffj
+
+            if npj.abs(overlap) < zero_th:
+#                jax.debug.print('continues {x}', x=True)
+                continue
+
+            Ga_j = gab(psia_i, psia_j)
+            if noccb > 0:
+                Gb_j = gab(psib_i, psib_j)
+            else:
+                Gb_j = npj.zeros_like(Ga_j)
+
+            G_j = [Ga_j, Gb_j]
+
+            rho = G_j[0].diagonal() + G_j[1].diagonal()
+            ke = npj.sum(T[0] * G_j[0] + T[1] * G_j[1])
+            pe = U * npj.dot(G_j[0].diagonal(), G_j[1].diagonal())
+            e_ph = w0 * npj.sum(beta_i * beta_j)
+            e_eph = -g * npj.dot(rho, beta_i + beta_j)
+
+            num_energy += (ke + pe + e_ph + e_eph) * overlap  # 2.0 comes from hermiticity
+            denom += overlap
+        
+#            jax.debug.print('overlap:   {x}', x=overlap)
+#            jax.debug.print('energy:    {x}', x=ke + pe + e_ph + e_eph)
+
+
+#    jax.debug.print('denom: {x}', x=denom)
+#    jax.debug.print('energy:{x}', x=num_energy)
+
+    etot = num_energy / denom
+#    jax.debug.print('etot:  {x}', x=etot)
+#    jax.debug.print('N2 scaling {x}', x=True)
+#    exit()
+
+#    etot = num_energy.real / denom.real
+    return etot.real
+
+
+def objective_function_toyozawa_mo(
+    x, nbasis, nup, ndown, T, U, g, m, w0, perms, coeffs, restricted, zero_th=1e-12
+):
+    nbasis = int(round(nbasis))
+    nup = int(round(nup))
+    ndown = int(round(ndown))
+
+    shift0 = x[:nbasis]
+
+    nbsf = nbasis
+    nocca = nup
+    noccb = ndown
+
+    psi0a = npj.array(x[nbsf : nbsf + nbsf * nocca], dtype=npj.float64)
+    psi0a = psi0a.reshape((nocca, nbsf)).T
+    coeff0 = coeffs[0].conj()
+
+    if noccb > 0:
+        psi0b = npj.array(x[nbsf + nbsf * nocca :], dtype=npj.float64)
+        psi0b = psi0b.reshape((noccb, nbsf)).T
+
+    num_energy = 0.0
+    denom = 0.0
+
+    #beta0 = shift0 * npj.sqrt(m * w0 / 2.0)
+    beta0 = shift0
+
+#    number_indep_ovlps = sum(divmod(nbasis, 2))
+    number_indep_ovlps = nbasis
+
+#    for ip, (perm, coeff) in enumerate(zip(perms, coeffs)):
+    for ip in range(number_indep_ovlps): 
+        perm = perms[ip]
+        coeff = coeffs[ip]
+
         beta_j = beta0[npj.array(perm)]
+        psia_j = psi0a[perm, :]
 
         if noccb > 0:
             psib_j = psi0b[perm, :]
@@ -70,8 +165,9 @@ def objective_function_toyozawa_mo(
             overlap = npj.linalg.det(psi0a.T.dot(psia_j)) * npj.prod(
                 npj.exp(-0.5 * (beta0**2 + beta_j**2) + beta0 * beta_j)
             )
+        overlap *= coeff0 * coeff
 
-        if overlap < zero_th:
+        if npj.abs(overlap) < zero_th:
             continue
 
         Ga_j = gab(psi0a, psia_j)
@@ -87,11 +183,31 @@ def objective_function_toyozawa_mo(
         pe = U * npj.dot(G_j[0].diagonal(), G_j[1].diagonal())
         e_ph = w0 * npj.sum(beta0 * beta_j)
         e_eph = -g * npj.dot(rho, beta0 + beta_j)
+        
+#        jax.debug.print('overlap:   {x}', x=overlap)
+#        jax.debug.print('energy:    {x}', x=ke + pe + e_ph + e_eph)
+        
+
+        if ip != 0:
+            overlap = overlap.real * (nbasis-ip) * 2
+        else:
+            overlap = overlap.real * nbasis
+
+#        if ip == 0:
+#            overlap = overlap.real * nbasis
+#        else:
+#            overlap = 2 * (nbasis - 2*ip) * overlap.real
 
         num_energy += (ke + pe + e_ph + e_eph) * overlap  # 2.0 comes from hermiticity
         denom += overlap
 
+
+#    jax.debug.print('denom: {x}', x=denom)
+#    jax.debug.print('energy:{x}', x=num_energy)
+
     etot = num_energy / denom
+#    jax.debug.print('etot:  {x}', x=etot)
+#    exit()
     return etot.real
 
 
@@ -112,12 +228,13 @@ def circ_perm(lst: np.ndarray) -> np.ndarray:
 
 
 def variational_trial_toyozawa(
-    shift_init: np.ndarray, electron_init: np.ndarray, hamiltonian, system, verbose=2
+    shift_init: np.ndarray, electron_init: np.ndarray, hamiltonian, system, K=0, verbose=2
 ):
     psi = electron_init.T.real.ravel()
     shift = shift_init.real
 
     perms = circ_perm(np.arange(hamiltonian.nsites))
+    coeffs = np.exp(1j * K * np.arange(hamiltonian.nsites))
 
     x = np.zeros((system.nup + system.ndown + 1) * hamiltonian.nsites)
     x[: hamiltonian.nsites] = shift.copy()
@@ -136,6 +253,7 @@ def variational_trial_toyozawa(
             hamiltonian.m,
             hamiltonian.w0,
             perms,
+            coeffs,
             False,
         ),
         jac=gradient_toyozawa_mo,
@@ -154,6 +272,32 @@ def variational_trial_toyozawa(
         },
     )
 
+#    maxiter = 500 
+#    minimizer_kwargs = { 
+#        "jac": True,
+#        "args": (float(hamiltonian.nsites),float(system.nup),float(system.ndown),hamiltonian.T,0.0,hamiltonian.g,hamiltonian.m,hamiltonian.w0,perms,coeffs,False),
+#        "options": {
+#            "maxls": 20, 
+#            "iprint": 2,
+#            "gtol": 1e-10,
+#            "eps": 1e-10,
+#            "maxiter": maxiter,
+#            "ftol": 1.0e-10,
+#            "maxcor": 1000,
+#            "maxfun": 15000,
+#            "disp": False,
+#        }  
+#    }   
+#
+#    res = basinhopping(
+#        func,
+#        x,  
+#        minimizer_kwargs=minimizer_kwargs,
+#        callback=print_fun,
+#        niter=maxiter,
+#        niter_success=3,
+#    )     
+#
     etrial = res.fun
     beta_shift = res.x[: hamiltonian.nsites]
     if system.ndown > 0:
@@ -167,3 +311,11 @@ def variational_trial_toyozawa(
         psi = psia
 
     return etrial, beta_shift, psi
+
+def print_fun(x: np.ndarray, f: float, accepted: bool):
+    print("at minimum %.4f accepted %d" % (f, int(accepted)))
+
+def func(x, nbasis, nup, ndown, T, U, g, m, w0, perms, coeffs, restricted):
+    f = objective_function_toyozawa_mo(x, nbasis, nup, ndown, T, U, g, m, w0, perms, coeffs, restricted)
+    df = gradient_toyozawa_mo(x, nbasis, nup, ndown, T, U, g, m, w0, perms, coeffs, restricted)
+    return f, df
