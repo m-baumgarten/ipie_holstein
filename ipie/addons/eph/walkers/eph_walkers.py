7456572fe657a4dd047acf255bfa0522a69cf74b
Moritz Baumgarten <mbaumgarten@fas.harvard.edu>
2024-05-02 16:40:49 -0400

    greens functions for permutations added to walkers

--- ipie/addons/eph/walkers/eph_walkers.py
+++ ipie/addons/eph/walkers/eph_walkers.py
@@ -96,19 +96,24 @@ class EPhWalkers(BaseWalkers):
 
         if hasattr(trial, "nperms"):
             shape = (self.nwalkers, trial.nperms)
+            shape_G = (self.nwalkers, self.nbasis, self.nbasis, trial.nperms)
         else:
             shape = self.nwalkers
+            shape_G = (self.nwalkers, self.nbasis, self.nbasis)
 
         self.ph_ovlp = numpy.zeros(shape, dtype=numpy.complex128)
         self.el_ovlp = numpy.zeros(shape, dtype=numpy.complex128)
         self.ovlp_perm = numpy.zeros(shape, dtype=numpy.complex128)
 
-        self.buff_names += ["ovlp_perm"]
-        self.buff_size = round(self.set_buff_size_single_walker() / float(self.nwalkers))
-        self.walker_buffer = numpy.zeros(self.buff_size, dtype=numpy.complex128)
+        self.Ga_perm = numpy.zeros(shape_G, dtype=numpy.complex128)
+        self.Gb_perm = numpy.zeros_like(self.Ga_perm)
 
         self.Ga = numpy.zeros((self.nwalkers, self.nbasis, self.nbasis), dtype=numpy.complex128)
         self.Gb = numpy.zeros_like(self.Ga)
+
+        self.buff_names += ["ovlp_perm"]
+        self.buff_size = round(self.set_buff_size_single_walker() / float(self.nwalkers))
+        self.walker_buffer = numpy.zeros(self.buff_size, dtype=numpy.complex128)
 
         self.ovlp = trial.calc_overlap(self)
 
@@ -163,3 +168,4 @@ class EPhWalkers(BaseWalkers):
 
     def reortho_batched(self):  # gpu version
         pass
+
