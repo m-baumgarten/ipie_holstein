removed bug in analytical dd1 gradients for iffdiagonal elph models

ipie/addons/eph/trial_wavefunction/variational/dd1.py
@@ -26,8 +26,8 @@ from ipie.addons.eph.trial_wavefunction.variational.toyozawa import (
     ToyozawaVariational,
 )
 
-# from ipie.addons.eph.hamiltonians.dispersive_phonons import DispersivePhononModel
-
+from ipie.addons.eph.hamiltonians.dispersive_phonons import DispersivePhononModel
+from numba import jit
 
 class dD1Variational(ToyozawaVariational):
     def __init__(
@@ -96,40 +96,6 @@ class dD1Variational(ToyozawaVariational):
         cs_ovlp *= cs_ovlp_norm
         return cs_ovlp
 
-    def shift_perm_tmp(self, x, nperm, i, j):
-        shift, c0a, c0b = self.unpack_x(x)
-        shift = np.squeeze(shift)
-        beta_i = npj.roll(shift, shift=(-nperm, -nperm), axis=(0, 1))
-        perm_mat = npj.roll(np.eye(self.ham.N), shift=-nperm, axis=0)
-
-        ovlp = npj.einsum("ki,km,mn,nj->ij", shift.conj(), perm_mat, shift, perm_mat.T)
-        return ovlp[i, j]
-
-    def cs_ovlp_tmp(self, x, nperm, i, j):
-        shift, c0a, c0b = self.unpack_x(x)
-        shift = npj.squeeze(shift)
-        beta_i = npj.roll(shift, shift=(-nperm, -nperm), axis=(0, 1))
-        return self.cs_overlap(shift, beta_i)[i, j]
-
-    def elph_tmp(self, x, nperm, i, j):
-        shift, c0a, c0b = self.unpack_x(x)
-        shift = npj.squeeze(shift)
-        beta_i = npj.roll(shift, shift=(-nperm, -nperm), axis=(0, 1))
-        psia_i = c0a[self.perms[nperm]]
-        cs_ovlp = self.cs_overlap(shift, beta_i)
-
-        Ga_i = npj.outer(c0a.conj(), psia_i)
-
-        el_ph_contrib = npj.einsum("ijk,ij,ki,ij->", self.ham.g_tensor, Ga_i, shift.conj(), cs_ovlp)
-        el_ph_contrib += npj.einsum("ijk,ij,kj,ij->", self.ham.g_tensor, Ga_i, beta_i, cs_ovlp)
-        return el_ph_contrib.real
-
-    def double_perm_tmp(self, x, nperm, i, j):
-        shift, c0a, c0b = self.unpack_x(x)
-        shift = npj.squeeze(shift)
-        beta_i = npj.roll(shift, shift=(-nperm, -nperm), axis=(0, 1))
-        return npj.einsum("ki,kj->ij", shift.conj(), beta_i)[i, j]
-
     @plum.dispatch
     def projected_energy(self, ham: GenericEPhModel, G: list, shift_i, shift_j):
 
@@ -152,21 +118,21 @@ class dD1Variational(ToyozawaVariational):
         local_energy = kinetic + el_ph_contrib + phonon_contrib
         return local_energy
 
-    #    @plum.dispatch
-    #    def projected_energy(self, ham: DispersivePhononModel, G: list, shift_i, shift_j):
-    #        cs_ovlp = self.cs_overlap(shift_i, shift_j)
-    #        kinetic = npj.sum((ham.T[0] * G[0] + ham.T[1] * G[1]) * cs_ovlp)
-    #
-    #        el_ph_contrib = npj.einsum('ijk,ij,ki,ij->', ham.g_tensor, G[0], shift_i.conj(), cs_ovlp)
-    #        el_ph_contrib += npj.einsum('ijk,ij,kj,ij->', ham.g_tensor, G[0], shift_j, cs_ovlp)
-    #        if self.sys.ndown > 0:
-    #            el_ph_contrib = npj.einsum('ijk,ij,ki,ij->', ham.g_tensor, G[0], shift_i.conj(), cs_ovlp)
-    #            el_ph_contrib += npj.einsum('ijk,ij,kj,ij->', ham.g_tensor, G[0], shift_j, cs_ovlp)
-    #
-    #        phonon_contrib = ham.w0 * npj.einsum('ij,j->', shift_i.conj() * shift_j, cs_ovlp.diagonal() * G[0].diagonal())
-    #        phonon_contrib += npj.einsum('ij,in,jn,n->', ham.ph_tensor, shift_i.conj(), shift_j, G[0].diagonal() * cs_ovlp.diagonal())
-    #        local_energy = kinetic + el_ph_contrib + phonon_contrib
-    #        return local_energy
+    @plum.dispatch
+    def projected_energy(self, ham: DispersivePhononModel, G: list, shift_i, shift_j):
+        cs_ovlp = self.cs_overlap(shift_i, shift_j)
+        kinetic = npj.sum((ham.T[0] * G[0] + ham.T[1] * G[1]) * cs_ovlp)
+
+        el_ph_contrib = npj.einsum('ijk,ij,ki,ij->', ham.g_tensor, G[0], shift_i.conj(), cs_ovlp)
+        el_ph_contrib += npj.einsum('ijk,ij,kj,ij->', ham.g_tensor, G[0], shift_j, cs_ovlp)
+        if self.sys.ndown > 0:
+            el_ph_contrib = npj.einsum('ijk,ij,ki,ij->', ham.g_tensor, G[0], shift_i.conj(), cs_ovlp)
+            el_ph_contrib += npj.einsum('ijk,ij,kj,ij->', ham.g_tensor, G[0], shift_j, cs_ovlp)
+
+        phonon_contrib = ham.w0 * npj.einsum('ij,j->', shift_i.conj() * shift_j, cs_ovlp.diagonal() * G[0].diagonal())
+        phonon_contrib += npj.einsum('ij,in,jn,n->', ham.ph_tensor, shift_i.conj(), shift_j, G[0].diagonal() * cs_ovlp.diagonal())
+        local_energy = kinetic + el_ph_contrib + phonon_contrib
+        return local_energy
 
     @plum.dispatch
     def projected_energy(self, ham: ExcitonPhononCavityElectron, G: list, shift_i, shift_j):
@@ -191,6 +157,8 @@ class dD1Variational(ToyozawaVariational):
         local_energy = kinetic + el_ph_contrib + phonon_contrib + ferm_ferm_contrib
         return local_energy
 
+
+    #@jit(nopython=True)
     def gradient(self, x, *args) -> np.ndarray:
         """For GenericEPhModel"""
         shift, c0a, c0b = self.unpack_x(x)
@@ -219,7 +187,6 @@ class dD1Variational(ToyozawaVariational):
 
             fac_i = overlap_degeneracy(self.ham, ip) * self.Kcoeffs[0].conj() * coeffi
 
-            # beta_i = shift[permi]
             beta_i = np.roll(shift, shift=(-ip, -ip), axis=(0, 1))
             psia_i = c0a[permi]  # [permi, :]
 
@@ -233,93 +200,97 @@ class dD1Variational(ToyozawaVariational):
             kin_tensor = Ga_i * self.ham.T[0]
             kin = np.sum(kin_tensor * cs_ovlp)
 
-            ovlp_i = np.einsum("i,i,i->", c0a.conj(), psia_i, cs_ovlp.diagonal())
-
-            d_shift_perm_r = np.einsum(
-                "ei,pj->peij", np.eye(self.ham.N), perm_mat.dot(shift).dot(perm_mat.T)
-            )
-            d_shift_perm_r += np.einsum("pi,je->peij", perm_mat.T.dot(shift.conj()), perm_mat)
-            #            d_shift_perm_r = np.zeros_like(d_shift_perm_r)
-
-            d_shift_perm_i = -1j * np.einsum(
-                "ei,pj->peij", np.eye(self.ham.N), perm_mat.dot(shift).dot(perm_mat.T)
-            )
-            d_shift_perm_i += 1j * np.einsum("pi,je->peij", perm_mat.T.dot(shift.conj()), perm_mat)
-            #            d_shift_perm_i = -1j * np.einsum('ei,pm,mn,nj->peij',np.eye(self.ham.N), perm_mat, shift,perm_mat.T)
-            #            d_shift_perm_i += 1j * np.einsum('ki,kp,ej->peij', shift.conj(),perm_mat, perm_mat.T)
-
-            d_cs_ovlp_r = np.einsum("peij,ij->peij", d_shift_perm_r, cs_ovlp)
-            d_cs_ovlp_r -= np.einsum("ei,pi,ij->peij", np.eye(self.ham.N), shift.real, cs_ovlp)
-            d_cs_ovlp_r -= 0.5 * np.einsum(
-                "pb,ej,jb,ij->peij", shift, perm_mat.T, perm_mat, cs_ovlp
-            )
-            d_cs_ovlp_r -= 0.5 * np.einsum(
-                "pb,bj,je,ij->peij", shift.conj(), perm_mat.T, perm_mat, cs_ovlp
-            )
-            #            d_cs_ovlp_r -= np.einsum('pb,bj,je,ij->peij', shift.real, perm_mat.T, perm_mat, cs_ovlp)
-
-            d_cs_ovlp_i = np.einsum("peij,ij->peij", d_shift_perm_i, cs_ovlp)
-            d_cs_ovlp_i -= np.einsum("ei,pi,ij->peij", np.eye(self.ham.N), shift.imag, cs_ovlp)
-            d_cs_ovlp_i += (
-                1j * 0.5 * np.einsum("pb,ej,jb,ij->peij", shift, perm_mat.T, perm_mat, cs_ovlp)
-            )
-            d_cs_ovlp_i -= (
-                1j
-                * 0.5
-                * np.einsum("pb,bj,je,ij->peij", shift.conj(), perm_mat.T, perm_mat, cs_ovlp)
-            )  # TODO
-            #            d_cs_ovlp_r -= np.einsum('pb,bj,je,ij->peij', shift.imag, perm_mat.T, perm_mat, cs_ovlp)
+            ovlp_i = np.sum(c0a.conj() * psia_i * cs_ovlp.diagonal())
 
             # shift_grad_real contribs
-            kin_contrib = np.einsum("ij,peij->pe", kin_tensor, d_cs_ovlp_r)
+            kin_csovlp = kin_tensor * cs_ovlp
+            kin_contrib = beta_i.dot(kin_csovlp.T)
+            kin_contrib += perm_mat.T.dot(shift.conj()).dot(kin_csovlp).dot(perm_mat) # TODO this rolls both indices of shift.conj().dot(kin_csovlp)
+            kin_contrib -= shift.real * np.sum(kin_csovlp, axis=1)
+#            kin_contrib -= (shift.real.dot(perm_mat.T) * np.sum(kin_csovlp, axis=0)).dot(perm_mat) # TODO this rolls sum kin_csovlp in both indices
+            kin_contrib -= shift.real * np.roll(np.sum(kin_csovlp, axis=0), shift=ip)
 
-            elph_ij_contracted = np.einsum("ijk,ij->ijk", self.ham.g_tensor, Ga_i)
-            el_ph_contrib = np.einsum(
-                "ijk,ki,peij->pe", elph_ij_contracted, shift.conj(), d_cs_ovlp_r
-            )
-            el_ph_contrib += np.einsum("ijk,kj,peij->pe", elph_ij_contracted, beta_i, d_cs_ovlp_r)
-            el_ph_contrib += np.einsum("ejp,ej->pe", elph_ij_contracted, cs_ovlp)
-            el_ph_contrib += np.einsum(
-                "ijk,ij,kp,ej->pe", elph_ij_contracted, cs_ovlp, perm_mat, perm_mat.T
-            )
+            elph_ij_contracted = self.ham.g_tensor * Ga_i[..., None]
+            elph_ij_contr_shift_cs = np.sum(elph_ij_contracted * shift.conj().T[:, None, :], axis=2) * cs_ovlp
+            el_ph_contrib = beta_i.dot(elph_ij_contr_shift_cs.T)
+            el_ph_contrib += perm_mat.T.dot(shift.conj()).dot(elph_ij_contr_shift_cs).dot(perm_mat) # NOTE If there is translational invariance, are all terms of H translationally invariant?
+            el_ph_contrib -= shift.real * np.sum(elph_ij_contr_shift_cs, axis=1)
+            el_ph_contrib -= (shift.real.dot(perm_mat.T) * np.sum(elph_ij_contr_shift_cs ,axis=0)).dot(perm_mat)
 
-            boson_contrib = np.einsum("ii,ki,peii->pe", Ga_i, shift.conj() * beta_i, d_cs_ovlp_r)
-            boson_contrib += np.einsum("ii,peii,ii->pe", Ga_i, d_shift_perm_r, cs_ovlp)
+            elph_ij_contracted_beta_cs = np.sum(elph_ij_contracted * beta_i.T[None, ...], axis=2) * cs_ovlp
+            el_ph_contrib += beta_i.dot(elph_ij_contracted_beta_cs.T)
+            el_ph_contrib += perm_mat.T.dot(shift.conj()).dot(elph_ij_contracted_beta_cs).dot(perm_mat)
+            el_ph_contrib -= shift.real * np.sum(elph_ij_contracted_beta_cs, axis=1)           
+            el_ph_contrib -= (shift.real.dot(perm_mat.T) * np.sum(elph_ij_contracted_beta_cs,axis=0)).dot(perm_mat)
+
+            el_ph_contrib += np.sum(elph_ij_contracted * cs_ovlp[...,None], axis=1).T
+            el_ph_contrib += np.sum((elph_ij_contracted * cs_ovlp[...,None]).dot(perm_mat).swapaxes(1,2).dot(perm_mat), axis=0)
+
+            Ga_shifts = Ga_i.diagonal() * cs_ovlp.diagonal()
+            ovlp_contrib = beta_i * Ga_shifts
+            ovlp_contrib += (perm_mat.T.dot(shift.conj()) * Ga_shifts).dot(perm_mat)
+            ovlp_contrib -= shift.real * Ga_shifts
+            ovlp_contrib -= (shift.real.dot(perm_mat.T) * Ga_shifts).dot(perm_mat)
+
+            Ga_shifts *= np.sum(shift.conj() * beta_i, axis=0)
+            boson_contrib = beta_i * Ga_shifts
+            boson_contrib += (perm_mat.T.dot(shift.conj()) * Ga_shifts).dot(perm_mat)
+            boson_contrib -= shift.real * Ga_shifts
+            boson_contrib -= (shift.real.dot(perm_mat.T) * Ga_shifts).dot(perm_mat)
+            boson_contrib += (beta_i * Ga_i.diagonal() * cs_ovlp.diagonal())
+            boson_contrib += (perm_mat.T.dot(shift.conj()) * Ga_i.diagonal() * cs_ovlp.diagonal()).dot(perm_mat)
             boson_contrib *= self.ham.w0
 
-            ovlp_contrib = np.einsum("i,peii->pe", Ga_i.diagonal(), d_cs_ovlp_r)
             sgr = kin_contrib + el_ph_contrib + boson_contrib
             sgr_ovlp = ovlp_contrib
 
             # shift_grad_imag contribs
-            kin_contrib = np.einsum("ij,peij->pe", kin_tensor, d_cs_ovlp_i)
+            kin_contrib = -1j * beta_i.dot(kin_csovlp.T)
+            kin_contrib += 1j * perm_mat.T.dot(shift.conj()).dot(kin_csovlp).dot(perm_mat)
+            kin_contrib -= shift.imag * np.sum(kin_csovlp, axis=1)
+            kin_contrib -= (shift.imag.dot(perm_mat.T) * np.sum(kin_csovlp, axis=0)).dot(perm_mat)
 
-            el_ph_contrib = np.einsum(
-                "ijk,ki,peij->pe", elph_ij_contracted, shift.conj(), d_cs_ovlp_i
-            )  # TODO contract over k already to 2 index object
-            el_ph_contrib += np.einsum("ijk,kj,peij->pe", elph_ij_contracted, beta_i, d_cs_ovlp_i)
-            el_ph_contrib -= 1j * np.einsum("ejp,ej->pe", elph_ij_contracted, cs_ovlp)
-            el_ph_contrib += 1j * np.einsum(
-                "ijk,ij,kp,ej->pe", elph_ij_contracted, cs_ovlp, perm_mat, perm_mat.T
-            )
+            el_ph_contrib = -1j * beta_i.dot(elph_ij_contr_shift_cs.T)
+            el_ph_contrib += 1j * perm_mat.T.dot(shift.conj()).dot(elph_ij_contr_shift_cs).dot(perm_mat)
+            el_ph_contrib -= shift.imag * np.sum(elph_ij_contr_shift_cs, axis=1)
+            el_ph_contrib -= (shift.imag.dot(perm_mat.T) * np.sum(elph_ij_contr_shift_cs, axis=0)).dot(perm_mat)
 
-            boson_contrib = np.einsum("ii,ki,peii->pe", Ga_i, shift.conj() * beta_i, d_cs_ovlp_i)
-            boson_contrib += np.einsum("ii,peii,ii->pe", Ga_i, d_shift_perm_i, cs_ovlp)
+            el_ph_contrib += -1j * beta_i.dot(elph_ij_contracted_beta_cs.T)
+            el_ph_contrib += 1j * perm_mat.T.dot(shift.conj()).dot(elph_ij_contracted_beta_cs).dot(perm_mat)
+            el_ph_contrib -= shift.imag * np.sum(elph_ij_contracted_beta_cs, axis=1)
+            el_ph_contrib -= (shift.imag.dot(perm_mat.T) * np.sum(elph_ij_contracted_beta_cs,axis=0)).dot(perm_mat)
+
+
+            el_ph_contrib -= 1j * np.sum(elph_ij_contracted * cs_ovlp[...,None], axis=1).T
+            el_ph_contrib += 1j *np.sum((elph_ij_contracted * cs_ovlp[...,None]).dot(perm_mat).swapaxes(1,2).dot(perm_mat), axis=0)
+
+            Ga_shifts = Ga_i.diagonal() * cs_ovlp.diagonal()
+            ovlp_contrib = -1j * beta_i * Ga_shifts
+            ovlp_contrib += 1j *(perm_mat.T.dot(shift.conj()) * Ga_shifts).dot(perm_mat)
+            ovlp_contrib -= shift.imag * Ga_shifts
+            ovlp_contrib -= (shift.imag.dot(perm_mat.T) * Ga_shifts).dot(perm_mat)
+            
+            Ga_shifts *= np.sum(shift.conj() * beta_i, axis=0)
+            boson_contrib = -1j * beta_i * Ga_shifts
+            boson_contrib += 1j * (perm_mat.T.dot(shift.conj()) * Ga_shifts).dot(perm_mat)
+            boson_contrib -= shift.imag * Ga_shifts
+            boson_contrib -= (shift.imag.dot(perm_mat.T) * Ga_shifts).dot(perm_mat)
+
+
+            boson_contrib -= 1j * (beta_i * Ga_i.diagonal() * cs_ovlp.diagonal())
+            boson_contrib += 1j * (perm_mat.T.dot(shift.conj()) * Ga_i.diagonal() * cs_ovlp.diagonal()).dot(perm_mat)
             boson_contrib *= self.ham.w0
 
-            ovlp_contrib = np.einsum("i,peii->pe", Ga_i.diagonal(), d_cs_ovlp_i)
             sgi = kin_contrib + el_ph_contrib + boson_contrib
             sgi_ovlp = ovlp_contrib
 
             # psia_grad_real contribs
-            g_contracted = np.einsum("ijk,ki->ij", self.ham.g_tensor, shift.conj())
-            g_contracted += np.einsum("ijk,kj->ij", self.ham.g_tensor, beta_i)
-            g_contracted = g_contracted * cs_ovlp
+            g_contracted = np.sum(self.ham.g_tensor * shift.conj().T[:, None, :], axis=2)
+            g_contracted += np.sum(self.ham.g_tensor * beta_i.T[None, ...], axis=2)
+            g_contracted *= cs_ovlp
             g_contracted = g_contracted.dot(perm_mat)
-            w_contracted = self.ham.w0 * np.einsum(
-                "ij,j,jn->jn", shift.conj() * beta_i, cs_ovlp.diagonal(), perm_mat
-            )
-            ovlp_contracted = np.einsum("i,ij->ij", cs_ovlp.diagonal(), perm_mat)
+            w_contracted = self.ham.w0 * perm_mat.T * (np.sum(shift.conj() * beta_i, axis=0) * cs_ovlp.diagonal())
+            ovlp_contracted = perm_mat.T * cs_ovlp.diagonal()
 
             kin_perm = (cs_ovlp * self.ham.T[0]).dot(perm_mat)
 
@@ -330,11 +301,11 @@ class dD1Variational(ToyozawaVariational):
                 g_contracted - g_contracted.T
             ).dot(c0a.imag)
             boson_contrib = (w_contracted + w_contracted.T).dot(c0a.real) + 1j * (
-                w_contracted - w_contracted.T
+                w_contracted.T - w_contracted
             ).dot(c0a.imag)
 
             ovlp_contrib = (ovlp_contracted + ovlp_contracted.T).dot(c0a.real) + 1j * (
-                ovlp_contracted - ovlp_contracted.T
+                ovlp_contracted.T - ovlp_contracted
             ).dot(c0a.imag)
             pgr = kin_contrib + el_ph_contrib + boson_contrib
             pgr_ovlp = ovlp_contrib
@@ -347,11 +318,11 @@ class dD1Variational(ToyozawaVariational):
                 g_contracted - g_contracted.T
             ).dot(c0a.real)
             boson_contrib = (w_contracted + w_contracted.T).dot(c0a.imag) - 1j * (
-                w_contracted - w_contracted.T
+                w_contracted.T - w_contracted
             ).dot(c0a.real)
 
             ovlp_contrib = (ovlp_contracted + ovlp_contracted.T).dot(c0a.imag) - 1j * (
-                ovlp_contracted - ovlp_contracted.T
+                ovlp_contracted.T - ovlp_contracted
             ).dot(c0a.real)
             pgi = kin_contrib + el_ph_contrib + boson_contrib
             pgi_ovlp = ovlp_contrib
@@ -367,11 +338,9 @@ class dD1Variational(ToyozawaVariational):
             psia_grad_real_ovlp += (fac_i * pgr_ovlp).real
             psia_grad_imag_ovlp += (fac_i * pgi_ovlp).real
 
-            eph_energy = np.einsum("ijk,ij,ki,ij->", self.ham.g_tensor, Ga_i, shift.conj(), cs_ovlp)
-            eph_energy += np.einsum("ijk,ij,kj,ij->", self.ham.g_tensor, Ga_i, beta_i, cs_ovlp)
-            ph_energy = self.ham.w0 * npj.einsum(
-                "ij,j->", shift.conj() * beta_i, cs_ovlp.diagonal() * Ga_i.diagonal()
-            )
+            eph_energy = np.sum(self.ham.g_tensor * (Ga_i * cs_ovlp)[..., None] * shift.conj().T[:, None, :])
+            eph_energy += np.sum(self.ham.g_tensor * (Ga_i * cs_ovlp)[..., None] * beta_i.T[None, ...])
+            ph_energy = self.ham.w0 * np.sum((shift.conj() * beta_i).dot(cs_ovlp.diagonal() * Ga_i.diagonal()))
             energy += (fac_i * (kin + eph_energy + ph_energy)).real
             ovlp += (fac_i * ovlp_i).real
 
@@ -384,4 +353,5 @@ class dD1Variational(ToyozawaVariational):
         dx_ovlp = self.pack_x(shift_grad_ovlp, psia_grad_ovlp)
 
         dx = dx_energy / ovlp - dx_ovlp * energy / ovlp**2
+        print('my grad: ', dx)
         return dx